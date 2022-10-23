23dc76ad41863158bf413778769958e2bcb69411
Harald Fielker <harald.fielker@gmail.com>
2022-10-23 17:00:12 +0200

    emrichen experiments

---
+++ experiments/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# tests for learning https://github.com/con2/emrichen
+
+emrichen --template-format yaml --output-file=./output/add-default.yaml --output-format yaml ./input/add.in.yaml
+emrichen --template-format yaml --define foo=bar --define apiServerAddress=9.9.9.9 --output-file=./output/add-cli.yaml --output-format yaml ./input/add.in.yaml
+emrichen --template-format yaml --var-file=./input/add.var.yaml --output-file=./output/add-var-file.yaml --output-format yaml ./input/add.in.yaml
